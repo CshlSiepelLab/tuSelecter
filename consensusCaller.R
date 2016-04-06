a71bf4e88fa833adefd936eb532388ed55b12655
ndukler <ned36@cornell.edu>
2016-04-06 19:30:37 -0400

    Draft scripts for calling consensus TUs from multiple conditions.

---
+++ consensusCaller.R
@@ -0,0 +1,127 @@
+#!/usr/bin/env Rscript
+
+## Takes the final_tus files from any number of inputs and converts it 
+## into consensus calls while reporting some statistics
+
+## Requires a tuFilePath file which contains the paths to the final TU calls in column 1 and the experimental 
+## design in columns 2..N. Label columns with whatever labels you choose.
+
+## Read in arguments
+suppressMessages(library(argparse))
+parser <- ArgumentParser(description="Select transcriptional models based on data.")
+parser$add_argument("pathList",help="File containing all paths to final_tus.txt.")
+parser$add_argument("out",help="Directory to write consensus calls to") 
+
+args <- parser$parse_args()
+
+source("consensusLib.R")
+dir.create(args$out)
+pFile=fread(args$pathList)
+
+## Save column names and rename
+oldNames=colnames(pFile)
+pFile = renameCols(pFile)
+
+## Determine which rows are replicates
+pFile=findRepl(pFile)
+
+## Assign unique ids to each row just to protect against later rearrangement
+pFile[,uid:=1:nrow(pFile)]
+
+## Read in all files
+tus=list()
+for(i in sort(unique(pFile$uid))){
+    tus[[i]]=fread(pFile[uid==i]$path,colClasses=c("character","numeric","numeric","character","character","factor","character","character","numeric","numeric","numeric","character","character"))
+}
+
+##
+## TUs
+##
+
+## Look at concordance between replicates, including untranslated calls
+## Note: Genes that are inactive in both replicates are not counted
+concord.rpl=list()
+for(i in sort(unique(pFile$rid))){
+    tus.rpl=tus[pFile$uid[pFile$rid==i]]
+    concord.rpl[[i]] <- calcTuConcordance(tus.rpl,include.untrans=TRUE)
+}
+
+## Roughly 84% of all calls are concordant between replicates including untranslated vs translated
+concord.rpl.dist=table(rbindlist(concord.rpl)$per)
+concord.rpl.dist/sum(concord.rpl.dist)
+mean.tu.rpl.agree=rbindlist(concord.rpl)[,mean(per),by=GENEID]
+setnames(mean.tu.rpl.agree,"V1","perTuRplAgree")
+## concord.rpl.dist[1]/sum(concord.rpl.dist)
+
+## Look at concordance between all runs, ignoring untranslated genes 
+concord.all <- calcTuConcordance(tus)
+## hist(concord.all$per)
+setnames(concord.all,"per","perTuAgree")
+
+
+##
+## Runthrough
+##
+
+## Look at concordance between replicates for runthrough, 
+## Note: Genes that are inactive in both replicates are not counted
+concord.rpl.rt=list()
+for(i in sort(unique(pFile$rid))){
+    tus.rpl.rt=tus[pFile$uid[pFile$rid==i]]
+    concord.rpl.rt[[i]] <- calcRtConcordance(tus.rpl.rt)
+}
+concord.rpl.dist.rt=table(rbindlist(concord.rpl.rt)$per)
+concord.rpl.dist.rt/sum(concord.rpl.dist.rt)
+ag.rt=rbindlist(concord.rpl.rt)[,agree:=(per==0 | per==1),]
+mean.rt.rpl.agree=ag.rt[,mean(agree),by=GENEID]
+setnames(mean.rt.rpl.agree,"V1","rtRplAgree")
+
+
+## Look at concordance across all runs
+concord.all.rt <- calcRtConcordance(tus)
+## hist(concord.all.rt$per,breaks=0:length(tus)/length(tus))
+setnames(concord.all.rt,"per","percent.rt")
+
+##
+## 5' and 3' ends
+##
+
+concord.rpl.co=list()
+for(i in sort(unique(pFile$rid))){
+    tus.rpl.co=tus[pFile$uid[pFile$rid==i]]
+    concord.rpl.co[[i]] <- calcCoordConcordance(tus.rpl.rt)
+}
+concord.rpl.all=rbindlist(concord.rpl.co)
+##hist(concord.rpl.all$fMAD,breaks=100)
+## Fraction of Tus that have different starts between replicates
+sum(concord.rpl.all$fMAD==0,na.rm=TRUE)/sum(!is.na(concord.rpl.all$fMAD))
+sum(concord.rpl.all$tMAD==0,na.rm=TRUE)/sum(!is.na(concord.rpl.all$tMAD))
+## Quantile distiribtion of MAD
+quantile(concord.rpl.all$fMAD,na.rm=TRUE,c(seq(0,0.9,0.05),seq(0.9,1,0.01)))
+quantile(concord.rpl.all$tMAD,na.rm=TRUE,c(seq(0,0.9,0.05),seq(0.9,1,0.01)))
+
+
+# concord.rpl.dist.rt/sum(concord.rpl.dist.rt)
+
+## Look at concordance across all runs
+concord.all.co <- calcCoordConcordance(tus)
+
+##
+## Merge all of these into one big table that can then be used (along with GOF) to pick consensus call
+##
+big=merge(concord.all,mean.tu.rpl.agree,by="GENEID",all=TRUE)
+big=merge(big,concord.all.rt,by="GENEID",all=TRUE)
+big=merge(big,mean.rt.rpl.agree,by="GENEID",all=TRUE) 
+big=merge(big,concord.all.co,by="GENEID",all.x=TRUE)
+
+## Filter by some criteria
+## Either the calls are highly concordant or they tend to have very close beginnings and endings
+big.filter=big[((perTuAgree>=0.8 & perTuRplAgree>=0.5) | (fMAD<=500 & tMAD <=500)) & percent.rt<=0.5,]
+
+### Produce consensus calls
+tx.db=unique(rbindlist(tus)[,1:8,with=FALSE])
+con.tx=tx.db[TXNAME %in% big.filter$consensus]
+
+## Write output
+write.table(big,file.path(args$out,"consensus_stats.txt"),row.names=FALSE)
+write.table(con.tx,file.path(args$out,"consensus_tus.txt"),row.names=FALSE)
---
+++ consensusLib.R
@@ -0,0 +1,126 @@
+#!/usr/bin/env Rscript
+
+## Library of functions to do consensus calling from multiple TuSelecter calls
+
+args=list()
+args$pathList="/sonas-hs/siepel/hpc_norepl/home/ndukler/Projects/celastrol/results/tu_selecter_new/finalTuPath.txt"
+args$parallel=2
+args$out="/sonas-hs/siepel/hpc_norepl/home/ndukler/Projects/celastrol/results/tu_selecter_new/consensus_tus"
+
+library(data.table)
+library(doParallel)
+
+## Rename columns so later code can assume column names
+renameCols <- function(pFile){
+    ## Rename columns for ease of use
+    if(ncol(pFile)>1){
+        setnames(pFile,oldNames,c("path",paste0("V",1:(length(oldNames)-1))))
+    } else {
+        setnames(pFile,oldNames,c("path"))
+    }
+    return(pFile)
+}
+
+## Determine which rows are replicates
+findRepl <- function(pFile){
+    ind=unique(pFile[,-1,with=FALSE])
+    ind[,rid:=1:nrow(ind)]
+    return(merge(pFile,ind,by=colnames(pFile[,-1,with=FALSE])))
+}
+
+## Wrapper function for merge
+mymerge <- function(x,y) {
+    return(merge(x,y,by="GENEID",suffixes=c(".x",".y")))
+}
+
+## Given a list of calls look at concordance
+calcTuConcordance <- function(sub.tus,include.untrans=FALSE,cores=args$parallel){
+    ## Bind together names for each replicate
+    temp=lapply(sub.tus,function(x) return (x[,c("GENEID","TXNAME"),with=FALSE]))
+    for(i in 1:length(temp)){
+        setnames(temp[[i]],colnames(temp[[i]])[2],paste0("V",i))    
+    }
+    txnames=Reduce(mymerge,temp)
+    ## Remove genes that are always untranscribed
+    include=which(apply(txnames[,-1,with=FALSE]=="Untranscribed",1,sum)<(ncol(txnames)-1))
+    ## Set up data.table to not count Untranscribed genes if that option is enabled
+    txnames=txnames[include]
+    if(include.untrans==FALSE){
+        txnames[txnames=="Untranscribed"]=NA
+    }
+
+    ## Split data by core
+    alloc=rep_len(x=1:cores, length.out=nrow(txnames))
+    split.tx=list()
+    for(i in 1:cores){
+        split.tx[[i]]=txnames[alloc==i]
+    }
+
+    con=rbindlist(mclapply(split.tx,function(x){
+        out=(rbindlist(apply(x,1,function(k) {
+            z=table(k[-1],useNA="no")
+            return(data.table(GENEID=k[1],consensus=names(z)[which.max(z)],per=max(z)/sum(z)))
+        })))
+        return(out)
+      },mc.cores=cores))
+    return(con)
+}
+
+
+## Computes how often RT calls agree based on a given alpha level
+calcRtConcordance <- function(sub.tus,alpha=0.05){
+    ## Bind together names for each replicate
+    temp=lapply(sub.tus,function(x) return (x[,c("GENEID","q"),with=FALSE]))
+    for(i in 1:length(temp)){
+        ## Convert all catagorical variables to 1="RT" or 0="non-RT"
+        temp[[i]]$q[temp[[i]]$q=="non-RT"]=0
+        temp[[i]]$q[temp[[i]]$q=="RT"]=1
+        temp[[i]]$q=as.numeric(temp[[i]]$q)
+        setnames(temp[[i]],colnames(temp[[i]])[2],paste0("V",i))
+    }
+    rt=Reduce(mymerge,temp)
+    ## Remove genes that are always untranscribed
+    include=which(apply(is.na(rt[,-1,with=FALSE]),1,sum)<(ncol(rt)-1))
+    ## Remove genes that are always untranscribed
+    rt=rt[include]
+
+    ## How many RT calls are there per row
+    hold=rt$GENEID
+    rt[rt<=alpha]=FALSE
+    rt[rt>alpha]=TRUE
+    rt$GENEID=hold
+    out=data.table(GENEID=rt$GENEID,per=apply(rt[,-1,with=FALSE],1,sum,na.rm=TRUE))
+    out[,per:=per/(ncol(rt)-1)]
+    return(out)
+}
+
+## Computes mean absolute deviation for the 5' and 3' ends of genes
+## Ignores untranscribed calls 
+calcCoordConcordance <- function(sub.tus){
+    ## Get gene ids for each row
+    info=data.table(GENEID=sub.tus[[1]]$GENEID,TXSTRAND=sub.tus[[1]]$TXSTRAND)
+    ## Bind together names for each replicate
+    txname=as.data.table(do.call("cbind", lapply(sub.tus,function(x) return (x$TXNAME))))
+    ## Bind together txstart for each replicate
+    start=as.data.table(do.call("cbind", lapply(sub.tus,function(x) return (x$TXSTART))))
+    ## Bind together txend ends for each replicate
+    end=as.data.table(do.call("cbind", lapply(sub.tus,function(x) return (x$TXEND))))
+
+    ## Set starts and ends of untranscribed transcripts to NA
+    start[txname=="Untranscribed"]=NA
+    end[txname=="Untranscribed"]=NA
+
+    ## Compute variance in starts and ends for all transcripts
+    sMAD=data.table(row=1:nrow(info),info,mad=apply(start,1,function(x) mean(abs(x-mean(x,na.rm=TRUE)),na.rm=TRUE)))
+    eMAD=data.table(row=1:nrow(info),info,mad=apply(end,1,function(x) mean(abs(x-mean(x,na.rm=TRUE)),na.rm=TRUE)))
+   
+    ## Transform start & end to 3' and 5' with respect to strand
+    fMAD=rbind(sMAD[TXSTRAND=="+"],eMAD[TXSTRAND=="-"])
+    fMAD=fMAD[order(row)]
+    fMAD[,row:=NULL]
+    tMAD=rbind(sMAD[TXSTRAND=="-"],eMAD[TXSTRAND=="+"])
+    tMAD=tMAD[order(row)]
+    tMAD[,row:=NULL]
+
+    return(data.table(GENEID=fMAD$GENEID,fMAD=fMAD$mad,tMAD=tMAD$mad))   
+}
